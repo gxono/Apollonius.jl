fa6794e9202eba65b199d0aff33b9479d9dbd449
gxono <57823100+gxono@users.noreply.github.com>
2026-09-24 01:12:35 -0300

    fix docs pics

--- docs/illustrations/polygons/pol_bb.jl
+++ docs/illustrations/polygons/pol_bb.jl
@@ -1,14 +1,23 @@
 include("../default_config.jl")
-lxm, lxo = @to_luxor_picture width=500 height=240 margin=20 begin
+lxm = @to_luxor_picture! width=500 height=240 margin=20 begin
     pg = APStraightNgon([APPoint(0.0, 0.0), APPoint(4.0, 0.0), APPoint(4.0, 3.0), APPoint(1.0, 3.0)])
 end
-(; pg) = lxo
+
 bb = APBoundingBox(pg)
+
 @svg_doc(lxm, @__FILE__, begin
+sethue(julia_purple)
 path(bb, action=:stroke)
 sethue(julia_blue)
 path(pg, action=:stroke)
-path(bbox_center(bb)); plot_point(julia_purple)
-path(vertices(pg)); plot_point(julia_blue)
-path([bb.min, bb.max]); plot_point(julia_purple)
+
+sethue("white")
+path(bbox_center(bb), action=:fillpreserve)
+sethue(julia_purple); strokepath()
+sethue("white")
+path(vertices(pg), action=:fillpreserve)
+sethue(julia_blue); strokepath()
+sethue("white")
+path([bb.min, bb.max], action=:fillpreserve)
+sethue(julia_purple); strokepath()
 end)
--- docs/illustrations/template.jl
+++
@@ -1,7 +0,0 @@
-include("default_config.jl")
-lxm, lxo = @to_luxor_picture width=500 height=240 margin=20 begin
-    # build the objects here: name = APObject(...)
-end
-# bring the fitted objects into scope: (; name1, name2) = lxo
-@svg_doc(lxm, @__FILE__, begin
-end)
